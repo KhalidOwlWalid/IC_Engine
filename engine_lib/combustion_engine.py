c272e31f201c6c1f71c5761985f1a6a4e105f202
KhalidOwlWalid <khalidowlwalid@gmail.com>
2026-07-05 11:39:10 +0100

    [feat] Working initial concept of piston animation

--- engine_lib/combustion_engine.py
+++ engine_lib/combustion_engine.py
@@ -18,42 +18,50 @@ def piston_speed_ratio(a, r, N, theta):
     u_p_ratio = (np.pi / 2) * np.sin(theta) * (1 + (np.cos(theta)/np.sqrt(np.power(R, 2) - np.power(np.sin(theta), 2))))
     return theta, u_p_ratio
 
-# theta = np.linspace(0, 2 * np.pi, 200)
-# a = 0.02
-# r = 0.05
-# N = 3000
-# theta_rad, u_p_ratio = piston_speed_ratio(a, r, N, theta)
+theta = np.linspace(0, 2 * np.pi, 200)
+a = 0.02
+r = 0.05
+N = 3000
+theta_rad, u_p_ratio = piston_speed_ratio(a, r, N, theta)
 
-# import matplotlib.patches as patches
-# import matplotlib.animation as animation
-# import matplotlib.transforms as tranforms
+import matplotlib.patches as patches
+import matplotlib.animation as animation
+import matplotlib.transforms as tranforms
 
-# fig, ax = plt.subplots()
-# ax.set_xlim(left=0, right=1.0)
-# ax.set_ylim(bottom=0, top=1.0)
-# width, height = 0.1, 0.05
-# block = patches.Rectangle((0.5, 0.5), width=width, height=height, fill=False, rotation_point="xy")
-# ax.add_patch(block)
+def main():
+    fig, ax = plt.subplots()
+    ax.set_xlim(left=0, right=1.0)
+    ax.set_ylim(bottom=0, top=1.0)
+    width, height = 0.1, 0.05
+    block = patches.Rectangle((0.5, 0.5), width=width, height=height, fill=False, rotation_point="xy")
+    block2 = patches.Rectangle((0.7, 0.5), width=width, height=height, fill=False, rotation_point="xy")
+    ax.add_patch(block)
+    ax.add_patch(block2)
 
-# t = np.linspace(0, 2*np.pi, 200)
+    t = np.linspace(0, 2*np.pi, 200)
 
-# def init():
-#     block.set_y(0)
-#     return(block,)
+    def init():
+        block.set(x=0.2)
+        block2.set_y(0)
+        return(block,block2,)
 
-# def animate(frame):
-#     y = np.sin(t[frame])
-#     block.set_y(y - height/2)
-#     return (block,)
+    def animate(frame):
+        y = np.sin(t[frame])
+        block.set_y(y - height/2)
+        block2.set_y(y - height/2 + 0.2)
+        return (block,block2,)
 
-# ani = animation.FuncAnimation(
-#     fig,
-#     animate,
-#     frames=len(t),
-#     init_func=init,
-#     interval=30,
-#     blit=True,
-#     repeat=True,
-# )
+    ani = animation.FuncAnimation(
+        fig,
+        animate,
+        frames=len(t),
+        init_func=init,
+        interval=30,
+        blit=True,
+        repeat=True,
+    )
 
-# plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.transforms as tf
+import matplotlib.animation as animation
 
 import engine_lib.combustion_engine as ic_eng
 
@@ -12,9 +13,6 @@ def main():
     ax.set_xlim(-1.0, 1.0)
     ax.set_ylim(-1.0, 1.0)
     ax.set_aspect("equal")
-
-
-    # Shape properties
 
     # Piston properties
     crank_offset = 0.2
@@ -26,33 +24,66 @@ def main():
     # Equation notation
     a = crank_offset
     r = connecting_rod_length
-    theta_deg = 270
-    theta_rad = np.deg2rad(theta_deg)
+    theta_deg = np.linspace(0, 360, 200)
 
-    # Connecting rod length bottom position
-    x = a * np.cos(np.deg2rad(90) - theta_rad)
-    y = a * np.sin(np.deg2rad(90) - theta_rad)
-
-    # Current piston position from wrist axis
-    s = ic_eng.get_current_piston_position(a, r, theta_rad)
-    alpha = np.arcsin((a/r) * np.sin(theta_rad))
-
+    x, y = 0, a
     crank_pin_coords = (x, y)
     crank_pin_width_offset = (x - cr_width/2, y)
-    connecting_rod = patches.Rectangle(crank_pin_width_offset, cr_width, cr_height, fill=True, rotation_point=crank_pin_coords, angle=np.rad2deg(alpha))
+    connecting_rod = patches.Rectangle(crank_pin_width_offset, cr_width, cr_height, fill=True, rotation_point=crank_pin_coords, angle=0)
 
     crankshaft_center = (0, 0)
     crank_pin_coords_with_offset = (crankshaft_center[0] - cp_width/2, crankshaft_center[1])
-    crank_pin = patches.Rectangle(crank_pin_coords_with_offset, cp_width, cp_height, fill=True, color="red", rotation_point=(0, 0), angle=-theta_deg)
+    crank_pin = patches.Rectangle(crank_pin_coords_with_offset, cp_width, cp_height, fill=True, color="red", rotation_point=(0, 0), angle=0)
 
     rod_path = patches.Circle((0, 0), radius=crank_offset, fill=False, linestyle="dashed")
+
     ax.add_patch(connecting_rod)
     ax.add_patch(crank_pin)
     ax.add_patch(rod_path)
 
     # Pivot point
-    ax.scatter(0, 0)
-    ax.scatter(crank_pin_coords[0], crank_pin_coords[1])
+    crank_pin_dot = ax.scatter([], [])
+
+    def init():
+        crank_pin.set(xy=(0, 0), angle=0)
+        connecting_rod.set(xy=(0 - cr_width/2, a))
+        connecting_rod.rotation_point = (0, a)
+        connecting_rod.angle = 0
+        return (crank_pin, connecting_rod, crank_pin_dot)
+
+    def update(frame):
+
+        theta_rad = np.deg2rad(theta_deg[frame])
+
+        # Connecting rod length bottom position
+        x = a * np.cos(np.deg2rad(90) - theta_rad)
+        y = a * np.sin(np.deg2rad(90) - theta_rad)
+
+        # Current piston position from wrist axis
+        alpha = np.arcsin((a/r) * np.sin(theta_rad))
+
+        crank_pin_coords = (x, y)
+        crank_pin_with_offset = (x - cr_width/2, y)
+        crankshaft_center = (0 - cp_width/2, 0)
+
+        crank_pin.set(xy=crankshaft_center, angle=-np.rad2deg(theta_rad))
+        connecting_rod.set_xy(crank_pin_with_offset)
+        connecting_rod.rotation_point = crank_pin_coords
+        connecting_rod.angle = np.rad2deg(alpha)
+        crank_pin_dot.set_offsets([crank_pin_coords[0], crank_pin_coords[1]])
+
+        return (crank_pin, connecting_rod, crank_pin_dot)
+
+    ani = animation.FuncAnimation(
+        fig,
+        update,
+        frames=len(theta_deg),
+        init_func=init,
+        interval=1,
+        blit=True,
+        repeat=True,
+    )
+
     plt.show()
 
 if __name__ == "__main__":
